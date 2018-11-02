d90b007dca7df16f15ba82be817fb1f773fc7e83
Bruno Ferreira <fs.brunoferreira@gmail.com>
2018-11-02 18:25:28 -0300

    Create container list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { Header } from 'react-native-elements';
-import configureStore from './store/configureStore';
 import { Provider } from 'react-redux';
+import ListScreen from './containers/ListScreen';
+import configureStore from './store/configureStore';
 
 const store = configureStore();
 
@@ -10,19 +9,7 @@ export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <View>
-          <Header
-            statusBarProps={{}}
-            leftComponent={{ icon: 'menu', color: '#fff' }}
-            centerComponent={{
-              text: 'Translation Mile',
-              style: { color: '#fff', fontSize: 20 }
-            }}
-            rightComponent={{ icon: 'home', color: '#fff' }}
-            outerContainerStyles={{ backgroundColor: '#8CB13E' }}
-            innerContainerStyles={{ justifyContent: 'space-between' }}
-          />
-        </View>
+        <ListScreen />
       </Provider>
     );
   }
