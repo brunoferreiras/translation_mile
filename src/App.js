aaf692683b2f216382b6af1d1caa5a3e579c139c
Bruno Ferreira <fs.brunoferreira@gmail.com>
2018-11-02 17:10:15 -0300

    Add react-native-elements and vector-icons

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { Header } from 'react-native-elements';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Header
+          statusBarProps={{}}
+          leftComponent={{ icon: 'menu', color: '#fff' }}
+          centerComponent={{
+            text: 'Translation Mile',
+            style: { color: '#fff', fontSize: 20 }
+          }}
+          rightComponent={{ icon: 'home', color: '#fff' }}
+          outerContainerStyles={{ backgroundColor: '#8CB13E' }}
+          innerContainerStyles={{ justifyContent: 'space-between' }}
+        />
+      </View>
+    );
+  }
+}
