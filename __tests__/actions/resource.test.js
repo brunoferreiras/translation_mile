08b145d65076663959aa510cc5a632a6a258a897
Bruno Ferreira <fs.brunoferreira@gmail.com>
2018-11-04 07:18:06 -0300

    Add tests in resource actions

--- __tests__/actions/resource.test.js
+++ __tests__/actions/resource.test.js
@@ -1,11 +1,19 @@
 import * as actions from '../../src/actions/resource';
 import * as types from '../../src/actions/types';
 
-describe('Actions Resource', () => {
+describe('Resource Actions', () => {
   it('should create an action to get resources', () => {
     const expectedAction = {
-      type: types.LOAD_RESOURCES
+      type: types.GET_RESOURCES
     };
-    expect(actions.loadResources()).toEqual(expectedAction);
+    expect(actions.getResources()).toEqual(expectedAction);
+  });
+
+  it('should create an action to save resources in redux', () => {
+    const expectedAction = {
+      type: types.SAVE_RESOURCES,
+      payload: {}
+    };
+    expect(actions.saveResources({})).toEqual(expectedAction);
   });
 });
