2da362e0f4d26bdc9a73c05624f03f2cf4245452
Bruno Ferreira <fs.brunoferreira@gmail.com>
2018-11-04 10:06:05 -0300

    Add tests resource saga

---
+++ __tests__/sagas/resource.js
@@ -0,0 +1,38 @@
+import { call, put, takeLatest } from 'redux-saga/effects';
+import * as types from '../../src/actions/types';
+import { getResourcesTest } from '../../src/api/resource';
+import resourceSaga, { get } from '../../src/sagas/resource';
+
+describe('Resource Saga', () => {
+  it('should to receive take latest', () => {
+    const generator = resourceSaga();
+
+    expect(generator.next().value).toEqual(
+      takeLatest(types.GET_RESOURCES, get)
+    );
+  });
+
+  it('should to get the resources from API', () => {
+    const generator = get();
+    expect(generator.next().value).toEqual(call(getResourcesTest));
+
+    expect(generator.next().value).toEqual(
+      put({
+        type: types.SAVE_RESOURCES,
+        payload: undefined
+      })
+    );
+  });
+
+  it('should return exception when happen error in API', () => {
+    const generator = get();
+    expect(generator.next().value).toEqual(call(getResourcesTest));
+
+    expect(generator.throw('error').value).toEqual(
+      put({
+        type: types.SAVE_RESOURCES_ERROR,
+        payload: undefined
+      })
+    );
+  });
+});
--- src/sagas/resource.js
+++ src/sagas/resource.js
@@ -2,14 +2,14 @@ import { call, put, takeLatest } from 'redux-saga/effects';
 import * as types from '../actions/types';
 import { getResources as getResourcesAPI, getResourcesTest} from '../api/resource';
 
-function* get(action) {
+export function* get(action) {
   try {
-    const response = getResourcesTest();
+    const response = yield call(getResourcesTest);
     yield put({ type: types.SAVE_RESOURCES, payload: response});
     // const response = yield call(getResourcesAPI);
     // yield put({ type: types.SAVE_RESOURCES, payload: response.data});
   } catch (error) {
-    console.log(error);
+    yield put({ type: types.SAVE_RESOURCES_ERROR, payload: error.message});
   }
 }
 
